d2709dfd58f0fa394061f75e0855bc1f93ce2ff7
iamsanyakovalchuk <sashkokoval0808@gmail.com>
2024-09-30 09:05:41 +0300

    Add files via upload

---
+++ Практична1.py
@@ -0,0 +1,9 @@
+first_name = "Олександр"
+last_name = "Ковальчук"
+yers = 16
+world = "Hellow world"
+
+print("Тип змінної ім'я : ",type(first_name))
+print("Тип змінної призвіщем : ",type(last_name))
+print("Тип змінної вік : ",type(yers))
+print("Тип змінної світ : ",type(world))
