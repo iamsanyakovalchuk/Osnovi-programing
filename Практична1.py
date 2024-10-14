11726148bc55eb541aa6c361d53f819ce52d3616
iamsanyakovalchuk <sashkokoval0808@gmail.com>
2024-10-14 09:34:52 +0300

    Update Практична1.py

--- Практична1.py
+++ Практична1.py
@@ -1,7 +1,7 @@
 first_name = "Олександр"
 last_name = "Ковальчук"
 yers = 16
-world = "Hellow world"
+world = "Hello world"
 
 print("Тип змінної ім'я : ",type(first_name))
 print("Тип змінної призвіщем : ",type(last_name))
